7f86c2731fb4982ae6a666b8578b5f3166202a4b
Ashwin V <38224436+ashwinvis98@users.noreply.github.com>
2019-02-20 14:41:23 +0530

    Add files via upload

---
+++ titanic-dataset/titanic.py
@@ -0,0 +1,116 @@
+#data analysis libraries 
+import numpy as np
+import pandas as pd
+
+#visualization libraries
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+#ignore warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+#import train and test CSV files
+train = pd.read_csv("../input/train.csv")
+test = pd.read_csv("../input/test.csv")
+
+#see a summary of the training dataset
+train.describe(include = "all")
+
+# fill missing age with mode age group for each title
+mr_age = train[train["Title"] == 1]["AgeGroup"].mode() #Young Adult
+miss_age = train[train["Title"] == 2]["AgeGroup"].mode() #Student
+mrs_age = train[train["Title"] == 3]["AgeGroup"].mode() #Adult
+master_age = train[train["Title"] == 4]["AgeGroup"].mode() #Baby
+royal_age = train[train["Title"] == 5]["AgeGroup"].mode() #Adult
+rare_age = train[train["Title"] == 6]["AgeGroup"].mode() #Adult
+
+age_title_mapping = {1: "Young Adult", 2: "Student", 3: "Adult", 4: "Baby", 5: "Adult", 6: "Adult"}
+
+
+for x in range(len(train["AgeGroup"])):
+    if train["AgeGroup"][x] == "Unknown":
+        train["AgeGroup"][x] = age_title_mapping[train["Title"][x]]
+        
+for x in range(len(test["AgeGroup"])):
+    if test["AgeGroup"][x] == "Unknown":
+        test["AgeGroup"][x] = age_title_mapping[test["Title"][x]]
+
+#Choosing the Best Model: I will be testing the following models with my training data
+#Gaussian Naive Bayes, Logistic Regression, Support Vector Machines, 
+#Decision Tree Classifier, Random Forest Classifier, KNN or k-Nearest Neighbors
+
+# Gaussian Naive Bayes
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+gaussian = GaussianNB()
+gaussian.fit(x_train, y_train)
+y_pred = gaussian.predict(x_val)
+acc_gaussian = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_gaussian)
+
+# Logistic Regression
+from sklearn.linear_model import LogisticRegression
+
+logreg = LogisticRegression()
+logreg.fit(x_train, y_train)
+y_pred = logreg.predict(x_val)
+acc_logreg = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_logreg)
+
+# Support Vector Machines
+from sklearn.svm import SVC
+
+svc = SVC()
+svc.fit(x_train, y_train)
+y_pred = svc.predict(x_val)
+acc_svc = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_svc)
+
+# Linear SVC
+from sklearn.svm import LinearSVC
+
+linear_svc = LinearSVC()
+linear_svc.fit(x_train, y_train)
+y_pred = linear_svc.predict(x_val)
+acc_linear_svc = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_linear_svc)
+
+# Perceptron
+from sklearn.linear_model import Perceptron
+
+perceptron = Perceptron()
+perceptron.fit(x_train, y_train)
+y_pred = perceptron.predict(x_val)
+acc_perceptron = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_perceptron)
+
+#Decision Tree
+from sklearn.tree import DecisionTreeClassifier
+
+decisiontree = DecisionTreeClassifier()
+decisiontree.fit(x_train, y_train)
+y_pred = decisiontree.predict(x_val)
+acc_decisiontree = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_decisiontree)
+
+# Random Forest
+from sklearn.ensemble import RandomForestClassifier
+
+randomforest = RandomForestClassifier()
+randomforest.fit(x_train, y_train)
+y_pred = randomforest.predict(x_val)
+acc_randomforest = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_randomforest)
+
+# KNN or k-Nearest Neighbors
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier()
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_val)
+acc_knn = round(accuracy_score(y_pred, y_val) * 100, 2)
+print(acc_knn)
+
